ff5b7681ed333c6af07c7257649e2ea23db86955
bryarcole <bryarcole@gmail.com>
2016-12-26 13:08:37 -0600

    Non-working while loop

--- javascript/AdventureGame/js/controllers/controller.js
+++ javascript/AdventureGame/js/controllers/controller.js
@@ -1,31 +1,88 @@
 function newGame(){
-
+    //Prompt for Class and name
     var characterName = $("#character-name").val();
     var characterClass = $("#character-class").val();
-    var characterAge = $("#character-age").val();
-    var player = new character(characterName, characterAge, characterClass);
+    var Player = new character(characterName, characterClass);
     
+    // Get class selection
     if(characterClass == "Hunter"){
-        player.hunter();
+        Player.hunter();
     }else if(characterClass == "Titan"){
-        player.titan();
+        Player.titan();
     }else if (characterClass == "Warlock"){
-        player.warlock();
+        Player.warlock();
     };
-    player.cardCreate();
+    //Show Class selection - Hide form
+    Player.cardCreate();
     $("form").hide();
+
+    //Phase one, add Damage per to character
     function oldManMeet (){
-        alert("You meet an old man on your journy...");
-        alert("Pick a weapon!");
+        console.log("You meet an old man on your journy...");
         var weaponSelection = prompt("Pick your weapon now");
-        if(weaponSelection == "Thorn"){
-            player.thorn();
-        }else if(weaponSelection == "Last Word"){
-            player.LastWord();
-        }
-    }
+        
+        if(weaponSelection == "Last Word"){
+            Player.lastWord();
+        }else if(weaponSelection == "Thorn"){
+            Player.thorn();
+        };
+        $(".card-block").append("<p> You have selected ", Player.weaponName ," don't go madd with power! </p>");
+
+
+    };
     $(".continue").click(function(){
         oldManMeet();
+        console.log(Player.weaponName + Player.damagePer);
     });
+
+    //Encounter first Boss
+    function firstBoss(){
+        $(".character-card").hide();
+        var fighting = true;
+        var hitRate = Player.accuracy;
+        var damage = Player.damagePer;
+        var totalDamage = 0;
+        var playerHealth = Player.health + Player.armour + (Player.agility / 2);
+        var Golg = new Golgoroth();
+
+        while(fighting){
+            if(hitRate > 30){
+                console.log("You got a hit on him!");
+                totalDamage += damage;
+                console.log(hitRate);
+                console.log(totalDamage)
+                if(totalDamage > Golg.health){
+                    console.log("Nice you killed Golgoroth!");
+                    fighting = false;
+                }else{
+                    hitRate = Player.accuracy;
+                    console.log(totalDamage)
+                };
+            }else{
+                console.log("You missed, Golg attacks!");
+                Golg.attack -= playerHealth;
+                console.log(totalDamage);
+                if(playerHealth <= 0){
+                    console.log("Oh no Golg killed you!");
+                    console.log(totalDamage)
+                    fighting = false;
+                }else{
+                    hitRate = Player.accuracy;
+
+                };
+            };
+        }
+    };
+    $("section").append($fightButton);
+
+    $(".fight").click(function(){
+        firstBoss();
+    })
+    
+
+    
+
 };
+
+
 
--- javascript/AdventureGame/js/model/characters.js
+++ javascript/AdventureGame/js/model/characters.js
@@ -1,43 +1,54 @@
 //Create Objet or player 
 
-var $button = "<div class='btn btn-default'>Click here to get things started!</div>"
-var $continueButton = "<div class='btn btn-default continue'>Click here to get things started!</div>"
+var $button = "<div class='btn btn-default'>Click here to get things started!</div>";
+var $continueButton = "<div class='btn btn-default continue'>Click here to get things started!</div>";
+var $fightButton = "<div class='btn btn-default fight'>Click here to start the show!</div>";
 
-var character = function(name, age, des){
+var p = $("<p></p>");
+
+var character = function(name, des){
+    this.health = 20;
     this.name = name;
-    this.age = age;
     this.des = des;
 };
 
 
 
+//Testing to make sure object works
 character.prototype.verify = function(){
-    $("section").append("<p>","You're character name is ", this.name, "You're characters age is, ", this.age, "And finally youre characters class is ", this.des, "</p>");
+    $("section").append("<p> You're character name is ", this.name, "You're characters age is, ", this.age, "And finally youre characters class is ", this.des, "</p>");
 };
 
 // Characters with cards
 
 character.prototype.hunter = function () {
+    this.agility = 9;
+    this.recovery = 2;
+    this.armour = 6;
     var $holderElem = "<div class='card character-card'><div class='card-block'><img class='card-img' src='https://danfasulo.files.wordpress.com/2014/09/destiny-goldengun.jpg'/></div></div>";
     this.classItem = "Cloak";
     this.cardCreate= function () {
         $("section").append($holderElem);
-        $("section").append("<p>", "You're character name is ", this.name, "And youre characters class is ", this.des, "</p>")
-        $(".card").append($continueButton)
+        $("section").append($continueButton)
     };
 };
 
 character.prototype.warlock = function () {
+    this.health = 5;
+    this.agility = 4;
+    this.recovery = 8;
     var $holderElem = "<div class='card character-card'><div class='card-block'><img class='card-img' src='http://vignette1.wikia.nocookie.net/video151/images/a/aa/Destiny_Legendary_Warlock_vs._Ogre_-_Gamescom_2014/revision/latest?cb=20140816182310'/></div></div>";
     this.classItem = "Bond";
     this.cardCreate = function () {
         $("section").append($holderElem);
-        $("section").append("<p>", "You're character name is ", this.name, "And youre characters class is ", this.des, "</p>")
-        $(".card").append($continueButton)
+        $("section").text
     };
 };
 
 character.prototype.titan = function () {
+    this.health = 8;
+    this.agility = 2;
+    this.recovery = 5;
     var $holderElem = "<div class='card character-card'><div class='card-block'><img class='card-img' src='http://static5.gamespot.com/uploads/screen_kubrick/1352/13527689/2653237-destiny_titan.jpg'/></div></div>";
     this.classItem = "Mark";
     this.cardCreate = function () {
@@ -47,13 +58,31 @@ character.prototype.titan = function () {
     };
 };
 
-
+//Weapons choice //
 character.prototype.thorn = function() {
-    this.damagePer = 22;
-    this.accuracy = Math.floor((Math.random() * 10));
+    this.weaponName = "Thorn";
+    this.damagePer = 6.6;
+    this.accuracy = Math.floor((Math.random() * 85));
 };
 
-character.prototype.LastWord = function(){
-    this.damagePer = 30;
-    this.accuracy = Math.floor((Math.random() * 10));
-}
+character.prototype.lastWord = function(){
+    this.weaponName = "The Last Word";
+    this.damagePer = 9.4;
+    this.accuracy = Math.floor((Math.random() * 70));
+};
+
+
+
+// bosses //
+
+var Golgoroth = function(){
+    this.attack = 8;
+    this.health = 28;
+};
+
+var Variks = function(){
+    this.attack = 11;
+    this.health = 22;
+
+};
+
